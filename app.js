13cd96bced37fdf68b455da591295a81480b7048
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-03-11 17:57:18 +0100

    Create proper database paths

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const Thing = require('./models/Thing');
+
 const app = express();
 
 app.use(express.json());
@@ -19,32 +21,25 @@ mongoose.connect('mongodb+srv://alexiaaj:testmongo1@cluster0.nnl3ggs.mongodb.net
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.post('/api/stuff', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Objet cree'
-  })
-})
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+  .then(() => res.status(201).json({ message: 'Objet enregistre !'}))
+  .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
 
 app.get('/api/stuff', (req, res, next) => {
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'Mon premier objet',
-      description: 'Les infos de mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 4900,
-      userId: 'qsomihvqios',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'Mon deuxième objet',
-      description: 'Les infos de mon deuxième objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 2900,
-      userId: 'qsomihvqios',
-    },
-  ];
-  res.status(200).json(stuff);
+  Thing.find()
+  .then(things => res.status(200).json(things))
+  .catch(error => res.status(400).json({error}));
 });
 
 module.exports = app
