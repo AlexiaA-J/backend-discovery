073f1e7cfd6668d9c15d1b7ea12ed4670a16bcb2
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-03-12 11:30:22 +0100

    Create routing logic

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const Thing = require('./models/Thing');
+const stuffRoutes = require('./routes/stuff')
 
 const app = express();
-
-app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,32 +12,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.json());
+app.use('/api/stuff', stuffRoutes)
+
 mongoose.connect('mongodb+srv://alexiaaj:testmongo1@cluster0.nnl3ggs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  });
-  thing.save()
-  .then(() => res.status(201).json({ message: 'Objet enregistre !'}))
-  .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/stuff', (req, res, next) => {
-  Thing.find()
-  .then(things => res.status(200).json(things))
-  .catch(error => res.status(400).json({error}));
-});
 
 module.exports = app
