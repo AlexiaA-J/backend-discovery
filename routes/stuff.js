e142d67f7a3ba1c8680fc53acba9402e5105e440
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-03-12 17:04:40 +0100

    Add auth to stuff routes

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const stuffCtrl = require('../controllers/stuff')
+const auth = require('../middleware/auth');
 
-router.post('/', stuffCtrl.createThing);
-router.put('/:id', stuffCtrl.modifyThing);
-router.delete('/:id', stuffCtrl.deleteThing);
-router.get('/:id', stuffCtrl.getOneThing);
-router.get('/', stuffCtrl.getAllThings);
+const stuffCtrl = require('../controllers/stuff');
+
+router.post('/', auth, stuffCtrl.createThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.get('/', auth, stuffCtrl.getAllThings);
 
 module.exports = router;
